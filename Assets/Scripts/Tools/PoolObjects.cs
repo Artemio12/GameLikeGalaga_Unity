ccfb4cf225bf2ffb53e9ebb0ef2a8a9be38fa618
Artem Lomakin <artemio16@yandex.ru>
2023-12-03 00:53:20 +0400

    new Update

--- Assets/Scripts/Tools/PoolObjects.cs
+++ Assets/Scripts/Tools/PoolObjects.cs
@@ -1,79 +1,79 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public sealed class PoolObjects<T> where T : Rigidbody
-{
-    private Transform container;
-
-    private T prefab;
-    private List<T> pool;
-    public bool AutoExpand { private get; set; }
-
-    public PoolObjects(Transform container)
+    public sealed class PoolObjects<T> where T : Rigidbody
     {
-        this.container = container;
-    }
+        private Transform container;
 
-    public PoolObjects(Transform container, T prefab, int count) : this(container)
-    {
-        this.prefab = prefab;
+        private T prefab;
+        private List<T> pool;
+        public bool AutoExpand { private get; set; }
 
-        this.CreatePool(count);
-    }
-
-    private void CreatePool(int count)
-    {
-        pool = new List<T>();
-        for (int i = 0; i < count; i++)
+        public PoolObjects(Transform container)
         {
-            this.pool.Add(CreateObject()); 
+            this.container = container;
         }
-    }
 
-    private T CreateObject(bool isActiveByDefolt = false)
-    {
-        var createdObject = GameObject.Instantiate(prefab, container); //Ñîçäàíèå îáúåêòà 
-        createdObject.gameObject.SetActive(isActiveByDefolt);  // âêëþ÷åíèå èëè âûêëþ÷åíèå ïî áóëåâîé ïåðåìåííîé
-        return createdObject;
-    }
-
-    public T GetFreeElement()
-    {
-        if (this.HasFreeElement(out T element)) return element;
-
-        if (this.AutoExpand) return this.CreateObject(true);
-
-        throw new System.Exception($"There is no free elements in pool of type{typeof(T)}");
-    }
-
-    public T GetFreeElement(Vector3 position)
-    {
-        var element = GetFreeElement();
-        element.transform.position = position;
-        return element;
-    }
-
-    public T GetFreeElement(Vector3 position, Quaternion rotation)
-    {
-        var element = GetFreeElement();
-        element.transform.position = position;
-        element.transform.rotation = rotation;
-        return element;
-    }
-
-    public bool HasFreeElement(out T element)
-    {
-        foreach (var mono in pool)
+        public PoolObjects(Transform container, T prefab, int count) : this(container)
         {
-            if (!mono.gameObject.activeInHierarchy)
+            this.prefab = prefab;
+
+            this.CreatePool(count);
+        }
+
+        private void CreatePool(int count)
+        {
+            pool = new List<T>();
+            for (int i = 0; i < count; i++)
             {
-                element = mono;
-                mono.gameObject.SetActive(true);
-                return true;
+                this.pool.Add(CreateObject());
             }
         }
-        element = null;
-        return false;
-    }
 
-}
+        private T CreateObject(bool isActiveByDefolt = false)
+        {
+            var createdObject = GameObject.Instantiate(prefab, container); //Ñîçäàíèå îáúåêòà 
+            createdObject.gameObject.SetActive(isActiveByDefolt);  // âêëþ÷åíèå èëè âûêëþ÷åíèå ïî áóëåâîé ïåðåìåííîé
+            return createdObject;
+        }
+
+        public T GetFreeElement()
+        {
+            if (this.HasFreeElement(out T element)) return element;
+
+            if (this.AutoExpand) return this.CreateObject(true);
+
+            throw new System.Exception($"There is no free elements in pool of type{typeof(T)}");
+        }
+
+        public T GetFreeElement(Vector3 position)
+        {
+            var element = GetFreeElement();
+            element.transform.position = position;
+            return element;
+        }
+
+        public T GetFreeElement(Vector3 position, Quaternion rotation)
+        {
+            var element = GetFreeElement();
+            element.transform.position = position;
+            element.transform.rotation = rotation;
+            return element;
+        }
+
+        public bool HasFreeElement(out T element)
+        {
+            foreach (var mono in pool)
+            {
+                if (!mono.gameObject.activeInHierarchy)
+                {
+                    element = mono;
+                    mono.gameObject.SetActive(true);
+                    return true;
+                }
+            }
+            element = null;
+            return false;
+        }
+
+    }
